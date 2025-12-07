2345b2227130cac25e4bb1f70819439a17c6bbbc
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-07 14:51:20 -0500

    client-lib-db.tk.lua server-lib-db.tk.lua: update M.apply_changes +5
    res/init/web/client-lib-db.tk.lua
      + apply_changes, get_numbers_with_synced
      - M.apply_changes
      ~ format_record, format_records
    res/init/web/server-lib-db.tk.lua
      + M.sync
      - M.apply_changes
    
    also: lib/santoku/make/clean.lua, lib/santoku/make/project/lib.tk.lua, lib/santoku/make/project/web.tk.lua, make.lua, res/init/web/client-lib-entry-sw.tk.lua, res/init/web/client-lib-routes.tk.lua, res/init/web/client-res-index.css, res/init/web/dockerfile, res/init/web/gitignore, res/init/web/make-common.lua, res/init/web/res-web-templates-app.html, res/init/web/res-web-templates-icons-sync-warn.svg, res/init/web/res-web-templates-number-item.html, res/init/web/res-web-templates-number-items.html, res/init/web/res-web-templates-pagination.html, res/init/web/res-web-templates-sw-body.html, res/init/web/server-lib-web-sync.lua, res/init/web/toku.sh, res/web/nginx.tk.conf, res/web/run.sh

--- res/init/web/server-lib-web-sync.lua
+++ res/init/web/server-lib-web-sync.lua
@@ -1,6 +1,5 @@
 local db = require("__NAME__.db.loaded")
 
-
 local auth = ngx.var.http_authorization
 if not auth then
   ngx.status = 401
@@ -10,17 +9,11 @@ if not auth then
 end
 
 local session = db.get_or_create_session(auth)
-
-
 local args = ngx.req.get_uri_args()
 local since = tonumber(args.since) or 0
 
 ngx.req.read_body()
 local changes = ngx.req.get_body_data()
-if changes then
-  db.apply_changes(session, changes)
-end
-
 
 ngx.header.content_type = "application/json"
-ngx.say(db.get_changes(session, since))
+ngx.say(db.sync(session, changes, since))
