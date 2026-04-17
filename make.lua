8ef798c9643bcf2ee58b0e481e3c968baf7125f0
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-16 23:48:42 -0400

    bump version to 0.0.212-1 and update dependencies

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
   name = "santoku-make",
-  version = "0.0.211-1",
+  version = "0.0.212-1",
   variable_prefix = "TK_MAKE",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.325-1",
+    "santoku >= 0.0.327-1",
     "santoku-fs >= 0.0.43-1",
-    "santoku-web >= 0.0.493-1",
+    "santoku-web >= 0.0.497-1",
     "santoku-system >= 0.0.62-1",
     "santoku-template >= 0.0.36-1",
     "santoku-mustache >= 0.0.14-1",
