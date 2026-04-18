0833ef4388a2b913abaef2b9fdead55eec226f7d
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-18 13:53:29 -0400

    bump to 0.0.213-1, update license and deps

--- make.lua
+++ make.lua
@@ -1,18 +1,18 @@
 local env = {
   name = "santoku-make",
-  version = "0.0.212-1",
+  version = "0.0.213-1",
   variable_prefix = "TK_MAKE",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku >= 0.0.327-1",
-    "santoku-fs >= 0.0.43-1",
-    "santoku-web >= 0.0.497-1",
-    "santoku-system >= 0.0.62-1",
-    "santoku-template >= 0.0.36-1",
-    "santoku-mustache >= 0.0.14-1",
-    "santoku-bundle >= 0.0.43-1",
+    "santoku >= 0.0.328-1",
+    "santoku-fs >= 0.0.45-1",
+    "santoku-web >= 0.0.498-1",
+    "santoku-system >= 0.0.63-1",
+    "santoku-template >= 0.0.38-1",
+    "santoku-mustache >= 0.0.16-1",
+    "santoku-bundle >= 0.0.45-1",
   },
 }
 
